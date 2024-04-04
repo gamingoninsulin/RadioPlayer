1309ddfac551fb13c7fa1281fcbbc5d8444dd190
GamingOnInsulin <gamingoninsulin@gmail.com>
2024-04-04 16:37:35 +0200

    update site for dumb ai

--- data/js/radio-javascript.js
+++ data/js/radio-javascript.js
@@ -6,14 +6,17 @@ var stationGrid = document.querySelector(".station-grid");
 function playStation(station) {
   selectedStation = station;
   if (station.name) {
-    nowPlayingText.textContent = "Now Playing: " + station.name;
+    nowPlayingText.textContent = station.name;
+    var activeImage = document.getElementById("radio-image"); // Ensure this is inside the loop
+    activeImage.classList.add("station-image");
+    activeImage.src = station.logo;
   } else {
     // Handle cases where station.name is missing
-    console.error("Missing station name");
+    console.error("Missing station name!");
   }
 
   audioPlayer.onerror = function handleAudioError(error) {
-    nowPlayingText.textContent = "Error: Failed to play station.";
+    nowPlayingText.textContent = "Error: Failed to play the radio.";
     console.error("Audio playback error:", error); // Log the error for debugging
   };
   audioPlayer.src = station.url;
@@ -25,21 +28,20 @@ function playStation(station) {
 var stationGrid = document.querySelector(".station-grid");
 
 stations.forEach(function (station, index) {
-  var stationItem = document.createElement("div");
+  var stationItem = document.getElementById("Rdiv");
   stationItem.classList.add("station-item");
   stationItem.dataset.stationIndex = index;
 
-  var stationImage = document.createElement("img"); // Ensure this is inside the loop
+  var stationImage = document.getElementById("Rimg"); // Ensure this is inside the loop
   stationImage.classList.add("station-image");
   stationImage.src = station.logo; // Replace with your logo paths
 
-  var stationName = document.createElement("span");
+  var stationName = document.getElementById("Rspan");
   stationName.classList.add("station-name");
   stationName.textContent = station.name;
 
   stationItem.appendChild(stationImage);
   stationItem.appendChild(stationName);
-
   stationGrid.appendChild(stationItem);
 
   stationItem.addEventListener("click", function () {
